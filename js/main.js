d7830ef189e4e3a27266508cb786a420c9cff8cf
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-02-14 15:33:03 +0100

    finalizzazione progetto

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const app = new Vue(
+    {
+        el: "#root",
+        data: {
+            todos: [
+                {
+                    text: 'Eat',
+                    done: false
+                },
+                {
+                    text: 'Sleep',
+                    done: false
+                },
+                {
+                    text: 'Code',
+                    done: false
+                },
+                {
+                    text: 'Repeat',
+                    done: false
+                },
+            ],
+            input: '',
+        },
+        mounted() {
+            this.$nextTick(() => this.$refs.testInput.focus())
+        },
+        methods: {
+            submit() {
+                if(this.input.trim().length > 0) {
+                    const newTodoObj = {
+                        text: this.input.trim(),
+                        done: false
+                    }
+                    this.todos.push(newTodoObj);
+                } 
+                this.input = '';
+            },
+
+            removeTodo() {
+                this.todos.splice(index, 1);
+            },
+
+            lineThrough(index) {
+                this.todos[index].done = !this.todos[index].done
+            }
+        }
+    }
+)
